ed486f04ff54f8231e865158d1ac83e86213dee3
twl <294801089@qq.com>
2019-07-22 02:11:37 -0800

    发布项目

---
+++ SPRINGMVC-01/src/main/java/cn/tedu/spring/User.java
@@ -0,0 +1,8 @@
+package cn.tedu.spring;
+
+public class User {
+	public User(){
+		System.out.println("User创建了User创建了User创建了User创建了User创建了User创建了User创建了");
+	}
+
+}
---
+++ SPRINGMVC-01/src/test/java/cn/tedu/spring/tests.java
@@ -0,0 +1,9 @@
+package cn.tedu.spring;
+
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+public class tests {
+	InternalResourceViewResolver in;
+
+}
